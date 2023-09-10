73ae6ca37034d612cb67a8e8707ed090683d729b
Jordan Doyle <jordan.doyle@ucdconnect.ie>
2023-09-10 17:23:28 +0100

    Updated sample apk and jimple samples. Added activity and fragment logs. Updated bash logs.

--- sign.sh
+++ sign.sh
@@ -10,43 +10,43 @@
 script=$(echo "$0" | rev | cut -d "/" -f1 | rev)
 
 function exit_cleanup {
-	[[ -f "$temp_file" ]] && rm "$temp_file"
-	temp_file_idsig="temp_file.apk.idsig"
-    [[ -f "$temp_file_idsig" ]] && rm "$temp_file_idsig"
+  [[ -f "$temp_file" ]] && rm "$temp_file"
+  temp_file_idsig="temp_file.apk.idsig"
+  [[ -f "$temp_file_idsig" ]] && rm "$temp_file_idsig"
 }
 
 if [ $# -ne 2 ]; then
-    echo "[ERROR] ($script) - Required arguments not provided."
+    echo "[ERROR] ($script:${LINENO}) - Required arguments not provided."
     exit 2
 fi
 
 if [[ ! -d "$ANDROID_HOME" ]]; then
-	echo "[ERROR] ($script) - Android home environment variable is not set."
+	echo "[ERROR] ($script:${LINENO}) - Android home environment variable is not set."
 	exit 3
 fi
 
 tool_versions=("$ANDROID_HOME"/build-tools/*/)
-build_tools=$(printf "%s\n" "${tool_versions[*]}" | sort -nr | head -n1)
+build_tools=$(printf "%s\n" "${tool_versions[@]}" | sort -nr | head -n1)
 if [[ ! -d "$build_tools" ]]; then
-	echo "[ERROR] ($script) - Android build tools directory ($build_tools) does not exist."
+	echo "[ERROR] ($script:${LINENO}) - Android build tools directory ($build_tools) does not exist."
 	exit 4
 fi
 
 zip_align="$build_tools/zipalign"
 apk_signer="$build_tools/apksigner"
 if [[ ! -f "$zip_align" ]] || [[ ! -f "$apk_signer" ]]; then 
-	echo "[ERROR] ($script) - zipalign ($zip_align) or apksigner ($apk_signer) cannot be found."
+	echo "[ERROR] ($script:${LINENO}) - zipalign ($zip_align) or apksigner ($apk_signer) cannot be found."
 	exit 5
 fi
 
 if [[ ! -f "$1" ]] || [[ "$1" != *.apk ]]; then
-	echo "[ERROR] ($script) - Input file ($1) does not exist."
+	echo "[ERROR] ($script:${LINENO}) - Input file ($1) does not exist."
 	exit 6
 fi
 apk_file="$1"
 
 if [[ ! -f "$2" ]]; then
-	echo "[ERROR] ($script) - Input key store file ($2) does not exist."
+	echo "[ERROR] ($script:${LINENO}) - Input key store file ($2) does not exist."
 	exit 7
 fi
 key="$2"
@@ -58,4 +58,4 @@ trap exit_cleanup EXIT
 $zip_align -f 4 "$apk_file" "$temp_file"
 $apk_signer sign --ks "$key" --ks-pass pass:android "$temp_file"
 cp "$temp_file" "$apk_file"
-echo "[INFO] ($script) - The app is signed and located in $apk_file."
+echo "[INFO] ($script:${LINENO}) - The app is signed and located in $apk_file."
--- src/main/java/phd/research/jimple/JimpleGenerator.java
+++ src/main/java/phd/research/jimple/JimpleGenerator.java
@@ -1,5 +1,7 @@
 package phd.research.jimple;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import soot.*;
 import soot.javaToJimple.DefaultLocalGenerator;
 import soot.jimple.*;
@@ -13,6 +15,10 @@ import java.util.List;
 
 public class JimpleGenerator {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(JimpleGenerator.class);
+
+    public static final String A_TAG = "<ACTIVITY>";
+    public static final String F_TAG = "<FRAGMENT>";
     public static final String M_TAG = "<METHOD>";
     public static final String C_TAG = "<CONTROL>";
 
@@ -29,6 +35,17 @@ public class JimpleGenerator {
     }
 
     public void generateInstrumentUnits() {
+        if (this.body.getMethod().getName().equals("onCreate")) {
+            Value messageValue = StringConstant.v(A_TAG + " Activity: ");
+            Local nameLocal = this.generateGetName();
+            Value appendedMessage = this.generateAppend(messageValue, nameLocal);
+            this.generatePrint(appendedMessage);
+        }
+
+        if (this.body.getMethod().getName().equals("onCreateView")) {
+            this.generateFragmentLink();
+        }
+
         String instrumentMessage = C_TAG + " Method: " + this.body.getMethod().getSignature() + " View: ";
         Value stringValue = StringConstant.v(instrumentMessage);
 
@@ -46,6 +63,120 @@ public class JimpleGenerator {
         }
 
         this.generatePrint(printMessage);
+    }
+
+    private void generateFragmentLink() {
+        RefType stringType = Scene.v().getSootClass("java.lang.String").getType();
+        Local appendedString = this.generateNewLocal(stringType);
+        SootClass builderClass = Scene.v().getSootClass("java.lang.StringBuilder");
+        RefType builderType = builderClass.getType();
+        NewExpr newBuilderExpr = Jimple.v().newNewExpr(builderType);
+        Local builderLocal = this.generateNewLocal(builderType);
+        this.units.add(Jimple.v().newAssignStmt(builderLocal, newBuilderExpr));
+
+        Value message = StringConstant.v(F_TAG + " Fragment: ");
+        SootMethod initMethod = builderClass.getMethod("void <init>(java.lang.String)");
+        SpecialInvokeExpr initExpr = Jimple.v().newSpecialInvokeExpr(builderLocal, initMethod.makeRef(), message);
+        InvokeStmt initBuilderStmt = Jimple.v().newInvokeStmt(initExpr);
+        this.units.add(initBuilderStmt);
+
+        Local nameLocal = this.generateGetName();
+        SootMethodRef appendRef = builderClass.getMethod("java.lang.StringBuilder append(java.lang.String)").makeRef();
+        VirtualInvokeExpr appendExpr =
+                Jimple.v().newVirtualInvokeExpr(builderLocal, appendRef, generateToString(nameLocal));
+        Local tmpLocal = this.generateNewLocal(builderType);
+        AssignStmt builderAppendStmt = Jimple.v().newAssignStmt(tmpLocal, appendExpr);
+        this.units.add(builderAppendStmt);
+
+        SootClass superClass = null;
+        SootMethod getActivityMethod = null;
+        int ifStmtIndex = 0;
+        Local activityLocal = null;
+
+        SootClass fragment = this.body.getMethod().getDeclaringClass();
+        if (fragment.hasSuperclass()) {
+            superClass = fragment.getSuperclass();
+            for (SootMethod method : superClass.getMethods()) {
+                if (method.getName().equals("getActivity")) {
+                    getActivityMethod = method;
+                    break;
+                }
+            }
+        } else {
+            LOGGER.warn("Fragment class '" + fragment.getName() + "' does not have superclass.");
+        }
+
+        if (getActivityMethod == null) {
+            LOGGER.warn("No 'getActivity' method found for fragment '" + fragment.getName() + ".");
+        }
+
+        if (superClass != null && getActivityMethod != null) {
+            Local thisLocal = this.body.getThisLocal();
+            VirtualInvokeExpr getActivityMethodCallExpr =
+                    Jimple.v().newVirtualInvokeExpr(thisLocal, getActivityMethod.makeRef());
+            RefType activityType = superClass.getType();
+            activityLocal = this.generateNewLocal(activityType);
+            AssignStmt activityAssignStmt = Jimple.v().newAssignStmt(activityLocal, getActivityMethodCallExpr);
+            this.units.add(activityAssignStmt);
+
+            ifStmtIndex = this.units.size();
+
+            Value messageAddition = StringConstant.v(" Activity: ");
+            appendExpr = Jimple.v().newVirtualInvokeExpr(builderLocal, appendRef, generateToString(messageAddition));
+            tmpLocal = this.generateNewLocal(builderType);
+            builderAppendStmt = Jimple.v().newAssignStmt(tmpLocal, appendExpr);
+            this.units.add(builderAppendStmt);
+
+            SootMethod getClassMethod = Scene.v().grabMethod("<java.lang.Object: java.lang.Class getClass()>");
+            VirtualInvokeExpr classCallExpr = Jimple.v().newVirtualInvokeExpr(activityLocal, getClassMethod.makeRef());
+            RefType classType = Scene.v().getSootClass("java.lang.Class").getType();
+            Local classLocal = this.generateNewLocal(classType);
+            AssignStmt classAssignStmt = Jimple.v().newAssignStmt(classLocal, classCallExpr);
+            this.units.add(classAssignStmt);
+
+            nameLocal = this.generateNewLocal(stringType);
+            SootMethod getNameMethod = Scene.v().grabMethod("<java.lang.Class: java.lang.String getName()>");
+            VirtualInvokeExpr nameMethodCallExpr = Jimple.v().newVirtualInvokeExpr(classLocal, getNameMethod.makeRef());
+            AssignStmt nameAssignStmt = Jimple.v().newAssignStmt(nameLocal, nameMethodCallExpr);
+            this.units.add(nameAssignStmt);
+
+            appendExpr = Jimple.v().newVirtualInvokeExpr(builderLocal, appendRef, generateToString(nameLocal));
+            tmpLocal = this.generateNewLocal(builderType);
+            builderAppendStmt = Jimple.v().newAssignStmt(tmpLocal, appendExpr);
+            this.units.add(builderAppendStmt);
+        }
+
+        SootMethod toStringMethod = builderClass.getMethod("java.lang.String toString()");
+        VirtualInvokeExpr builderToStringExpr = Jimple.v().newVirtualInvokeExpr(builderLocal, toStringMethod.makeRef());
+        AssignStmt builderToStringStmt = Jimple.v().newAssignStmt(appendedString, builderToStringExpr);
+        this.units.add(builderToStringStmt);
+
+        this.generatePrint(appendedString);
+
+        if (superClass != null && getActivityMethod != null && activityLocal != null) {
+            Value nullValue = NullConstant.v();
+            EqExpr equalExpression = Jimple.v().newEqExpr(activityLocal, nullValue);
+            IfStmt ifStatement = Jimple.v().newIfStmt(equalExpression, builderToStringStmt);
+            units.add(ifStmtIndex, ifStatement);
+        }
+    }
+
+    private Local generateGetName() {
+        Local thisLocal = this.body.getThisLocal();
+        SootMethod getClassMethod = Scene.v().grabMethod("<java.lang.Object: java.lang.Class getClass()>");
+        VirtualInvokeExpr classMethodCallExpr = Jimple.v().newVirtualInvokeExpr(thisLocal, getClassMethod.makeRef());
+        RefType classType = Scene.v().getSootClass("java.lang.Class").getType();
+        Local classLocal = this.generateNewLocal(classType);
+        AssignStmt classAssignStmt = Jimple.v().newAssignStmt(classLocal, classMethodCallExpr);
+        this.units.add(classAssignStmt);
+
+        RefType stringType = Scene.v().getSootClass("java.lang.String").getType();
+        Local nameLocal = this.generateNewLocal(stringType);
+        SootMethod getNameMethod = Scene.v().grabMethod("<java.lang.Class: java.lang.String getName()>");
+        VirtualInvokeExpr nameMethodCallExpr = Jimple.v().newVirtualInvokeExpr(classLocal, getNameMethod.makeRef());
+        AssignStmt nameAssignStmt = Jimple.v().newAssignStmt(nameLocal, nameMethodCallExpr);
+        this.units.add(nameAssignStmt);
+        return nameLocal;
     }
 
     private void generatePrint(Value message) {
